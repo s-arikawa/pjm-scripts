66bc593a576f916d612a720a53b7ed92923a4874
s-arikawa <arikawa@i3-systems.com>
2023-01-05 14:41:31 +0900

    スプリントの課題一覧取得：スプリントIDをオプション（-s）で指定できるようにした。

--- src/jira/sprint-issues.mjs
+++ src/jira/sprint-issues.mjs
@@ -5,10 +5,14 @@
  * 実行方法: `zx src/jira/sprint-issues.mjs | pbcopy`
  * 説明: `pbcopy`コマンドでクリップボードにサマリー文字列(markdown)が保存されるので、Confluenceページにペーストすると良い感じにしてくれる。
  * 前提: .envにJIRA接続設定をしておく。
+ *
+ * オプション: `-s` 対象のスプリントIDを指定する。指定が無い場合は、実行時にActiveなスプリントを対象とする。
+ *           `zx src/jira/sprint-issues.mjs -s 963`
+ *           スプリントIDは、get-sprint-id.mjsを使うと簡単に確認可能。
  */
 
 import 'zx/globals'
-import { getIssuesForSprint, getSprintsFromBoard } from './jira-functions.mjs'
+import {getIssuesForSprint, getSprintsFromBoard} from './jira-functions.mjs'
 
 $.verbose = false
 require('dotenv').config()
@@ -23,11 +27,17 @@ const getActiveSprints = async (boardId) => {
 
 const boardId = process.env.JIRA_BOARD_ID
 
-// アクティブなスプリントIDを決める
-const activeSprint = await getActiveSprints(boardId)
+// 取得したいスプリントのIDをコマンドオプションで指定できる。
+let targetSprintId = argv.s // -s option
+
+if (!targetSprintId) {
+  // コマンドオプションで指定が無い場合は、アクティブなスプリントIDを取得して使用する
+  const activeSprint = await getActiveSprints(boardId)
+  targetSprintId = activeSprint.id
+}
 
 // スプリントに紐付く課題の一覧を取得する
-const issues = await getIssuesForSprint(activeSprint.id)
+const issues = await getIssuesForSprint(targetSprintId)
 
 // 課題の一覧をサマリーする
 const summary = issues
